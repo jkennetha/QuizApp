663c80548133d42c2ea534d64265936681aebe53
jkantonio.25 <jkantonio.25@gmail.com>
2019-04-23 16:52:02 +0800

    working quiz add and course add

--- quizmaker.php
+++ quizmaker.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE HTML>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
@@ -14,7 +14,56 @@
   </head>
   <body>
 	
-	
+<?php
+	if( ! empty($_POST['submit_course']) ){
+		$mysqli = new mysqli( 'localhost', 'root', '', 'quizapp');
+		if( $mysqli->connect_error){
+			die( 'Connect Error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect_error);
+		}
+
+		$sql = "INSERT INTO course (Course_Name, Course_Description, Course_isOpen) VALUES ('{$mysqli->real_escape_string($_POST['courseName'])}','{$mysqli->real_escape_string($_POST['courseDesc'])}', '1')";
+
+		$insert = $mysqli->query($sql);
+
+		if($insert) {
+			$message = "Success! Course: {$mysqli->real_escape_string($_POST['courseName'])} with ID: {$mysqli->insert_id} has been added";
+			echo "<script type='text/javascript'>alert('$message');</script>";
+		} else {
+			die("Error: {$mysqli->errno} : {$mysqli->error}");
+		}
+
+		$mysqli->close();
+	}else{
+
+	}
+
+	if( ! empty($_POST['submit_quiz']) ){
+		$mysqli = new mysqli( 'localhost', 'root', '', 'quizapp');
+		if( $mysqli->connect_error){
+			die( 'Connect Error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect_error);
+		}
+
+		$sql = "INSERT INTO quiz (Quiz_Name, Quiz_Description, Quiz_Difficulty, Quiz_PassingScore, Course_ID) VALUES ('{$mysqli->real_escape_string($_POST['quizName'])}','{$mysqli->real_escape_string($_POST['quizDesc'])}','{$mysqli->real_escape_string($_POST['quizDiff'])}','{$mysqli->real_escape_string($_POST['quizPass'])}','{$mysqli->real_escape_string($_POST['quizCourseID'])}')";
+
+		$insert = $mysqli->query($sql);
+
+		if($insert) {
+			$message = "Success! Quiz: {$mysqli->real_escape_string($_POST['quizName'])} with ID: {$mysqli->insert_id}, under Course ID: {$mysqli->real_escape_string($_POST['quizCourseID'])} has been added";
+			echo "<script type='text/javascript'>alert('$message');</script>";
+		} else {
+			die("Error: {$mysqli->errno} : {$mysqli->error}");
+		}
+
+		$mysqli->close();
+	}else{
+
+	}
+?>
+
+
+
+
+
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<a class="navbar-brand" href="welcome.php">Quiz App</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,21 +85,47 @@
 		</form>
 	</nav>
 	
-	<div class="card" style="width: 50rem;">
+	<div class="card" style="width: 20rem;">
 	
 	<div class="card-body">
-						<h5 class="card-title">CREATE A COURSE:</h5>
-						
-						<div class="form-group">
-							<label for="formGroupExampleInput">Name</label>
-							<input type="text" class="form-control" id="formGroupExampleInput">
-						</div>
-						<div class="form-group">
-							<label for="formGroupExampleInput2">Description</label>
-							<textarea class="form-control" id="validationTextarea"></textarea>
-						</div>
-							<button class="btn btn-primary">CREATE COURSE</button>
-					
+		<h5 class="card-title">CREATE A COURSE:</h5>
+		<form method="post" action="quizmaker.php">
+		<div class="form-group">
+			<label for="formGroupExampleInput">Name</label>
+			<input type="text" name="courseName" class="form-control" id="courseName">
+		</div>
+		<div class="form-group">
+			<label for="formGroupExampleInput2">Description</label>
+			<input type="text" name="courseDesc" class="form-control" id="courseDesc">
+		</div>
+			<input type="submit" name="submit_course" class="btn btn-primary" value="CREATE COURSE">
+		</form>
+	</div>
+	<div class="card-body">
+		<h5 class="card-title">CREATE A QUIZ:</h5>
+		<form method="post" action="quizmaker.php">
+			<div class="form-group">
+				<label for="formGroupExampleInput2">Course ID</label>
+				<input type="text" name="quizCourseID" class="form-control" id="quizCourseID">
+			</div>
+			<div class="form-group">
+				<label for="formGroupExampleInput">Name</label>
+				<input type="text" name="quizName" class="form-control" id="quizName">
+			</div>
+			<div class="form-group">
+				<label for="formGroupExampleInput2">Description</label>
+				<input type="text" name="quizDesc" class="form-control" id="quizDesc">
+			</div>
+			<div class="form-group">
+				<label for="formGroupExampleInput2">Difficulty</label>
+				<input type="text" name="quizDiff" class="form-control" id="quizDiff">
+			</div>
+			<div class="form-group">
+				<label for="formGroupExampleInput2">Passing Score</label>
+				<input type="text" name="quizPass" class="form-control" id="quizPass">
+			</div>
+				<input type="submit" name="submit_quiz" class="btn btn-primary" value="CREATE QUIZ">
+		</form>
 	</div>
 	
 	</div>
